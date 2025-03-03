b71316ce6f04cc6ae3440eb5473127de21e35a1e
7aider101-cse <500504212.hk@gmail.com>
2025-03-03 12:25:15 +0300

    Create congratulations.php

---
+++ congratulations.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Congratulations!</title>
+    <!-- Link to external CSS file -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Congratulations!</h1>
+        <p>You answered correctly.</p>
+    </div>
+</body>
+</html>
